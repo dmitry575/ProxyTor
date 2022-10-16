541dd0e766352c3e6558106f2296f84be8fc14e8
Dmitry Tarakanov <mail.dimm@gmail.com>
2022-10-16 12:22:52 +0300

    add example clean docker

--- docker-proxy.cmd
+++ docker-proxy.cmd
@@ -1,3 +1,4 @@
 docker-compose -f docker-compose.yml down
 docker-compose -f docker-compose.yml build proxy_tor
-docker-compose -f docker-compose.yml up -d
+#docker system prune -a
+docker-compose -f docker-compose.yml up -d 
